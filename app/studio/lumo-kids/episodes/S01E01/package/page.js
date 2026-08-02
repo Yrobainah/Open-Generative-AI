d3745352bfbfe3a0ea5c1f12a38fab6334ce8544
Yariel <69661560+Yrobainah@users.noreply.github.com>
2026-08-02 13:19:23 +0200

    Add S01E01 launch package route

---
+++ app/studio/lumo-kids/episodes/S01E01/package/page.js
@@ -0,0 +1,10 @@
+import LumoEpisodeLaunchPackagePageClient from './LumoEpisodeLaunchPackagePageClient';
+
+export const metadata = {
+  title: 'Paquete S01E01 | Lumo Kids Studio',
+  description: 'Estrategia multiplataforma de Lumo y la luz prestada.',
+};
+
+export default function LumoEpisodeLaunchPackagePage() {
+  return <LumoEpisodeLaunchPackagePageClient />;
+}
